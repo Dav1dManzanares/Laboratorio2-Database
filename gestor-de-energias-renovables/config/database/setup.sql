7c50a66aaac9efd675a49aa1b2e48e6d8ffdfdd5
Humbert <140095731+HumbertoCode@users.noreply.github.com>
2025-04-06 23:35:01 -0600

    Delete gestor-de-energias-renovables directory

--- gestor-de-energias-renovables/config/database/setup.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE IF NOT EXISTS gestor_energias;
-CREATE USER IF NOT EXISTS 'gestor_user'@'localhost' IDENTIFIED BY 'gestor_password';
-GRANT ALL PRIVILEGES ON gestor_energias.* TO 'gestor_user'@'localhost';
-FLUSH PRIVILEGES;
